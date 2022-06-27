a6f68dc29ba3fff59fd5e1b2e81467d1c1c0f839
ddrdev <russo.eduardo30@gmail.com>
2022-06-27 18:28:23 -0300

    Adicionadas bibliotecas e esqueleto do código

---
+++ main.py
@@ -0,0 +1,12 @@
+from weasyprint import HTML, CSS
+from jinja2 import Environment, FileSystemLoader
+import json
+from datetime import datetime
+from typing import Dict, List
+import locale
+
+def main() -> None:
+    pass
+
+if __name__ == '__main__':
+    main()
