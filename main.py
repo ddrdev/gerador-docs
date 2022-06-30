01438dfde107888b2061dd80857d476ba9b44ff8
ddrdev <russo.eduardo30@gmail.com>
2022-06-29 21:09:35 -0300

    Gera QR code e o adiciona ao rodapé

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from typing import Dict, List
 import locale
 from pathlib import Path
 import os
+import qrcode
 
 def convert_date(value : str) -> str:
     return datetime.strptime(value, "%B de %Y").strftime("%b/%Y") if value else "Presente"
@@ -16,11 +17,23 @@ def date_format(value : Dict) -> str:
 def concat_items(values : List, sep : str = ", ") -> str:
     return sep.join(values)
 
+def generate_qrcode(url : str) -> None:
+    qr = qrcode.QRCode(version=1,box_size=10,border=4)
+    qr.add_data(url)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    qrcode_path = Path('qrcode')
+    qrcode_path.mkdir(exist_ok=True)
+    filename = 'qrcode.png'
+    img.save(qrcode_path / filename)
+
 def main() -> None:
     locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
     input_path = Path('data.json')
     output_path = Path('output')
+    
+    resume_url = 'https://github.com/ddrdev/gerador-docs'
 
     with open(input_path, 'r', encoding="utf-8") as input_file:
         data = json.load(input_file)
@@ -37,6 +50,8 @@ def main() -> None:
         css_template = env.get_template('style.css')
 
         gen_date=datetime.today().strftime("%d de %B de %Y às %H:%M:%S")
+
+        generate_qrcode(resume_url)
 
         page = template.render(data=data)
         stylesheet = css_template.render(gen_date=gen_date)
